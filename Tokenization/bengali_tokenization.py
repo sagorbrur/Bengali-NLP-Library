60e334f7de70f44a87857b708436d1880b0690b9
Sagor Sarker <sagorhem3532@gmail.com>
2019-07-21 11:42:50 +0600

    Update bengali_tokenization.py

--- Tokenization/bengali_tokenization.py
+++ Tokenization/bengali_tokenization.py
@@ -4,7 +4,7 @@ import re
 
 sentence = "রাজপণ্ডিত হব মনে আশা করে। সপ্তশ্লোক ভেটিলাম রাজা গৌড়েশ্বরে। শত কষ্টে রাজা একদিন হারিয়ে গেলেন...... কে ফেরাবে তাকে! শত চেষ্টাতেও রাণী তাকে ফেরাতে পারলেন না।"
 
-input_text = re.sub(r'[,.:@#?!&$।]', ' ', sentence)
+input_text = re.sub(r'[,.:@#?!&$।\(\)\[\]\{\}\<\>/|\+\-\*\=\`\~\%\^\\\;\ঃ]', ' ', sentence)
 # print(input_text)
 tokens = input_text.split()
 print(tokens)
