7ccd207d3a459ed0289ce1228bf784d261eaf274
Sagor Sarker <sagorhem3532@gmail.com>
2019-07-22 09:53:59 +0600

    Update bengali_tokenization.py

--- Tokenization/bengali_tokenization.py
+++ Tokenization/bengali_tokenization.py
@@ -1,15 +1,26 @@
-# Bengali Tokenization using python and regular expression
+# bengali tokenization using nltk. It's not perfect but works good.
 
+from nltk.tokenize import word_tokenize
 import re
+from string import punctuation
 
-sentence = "রাজপণ্ডিত হব মনে আশা করে। সপ্তশ্লোক ভেটিলাম রাজা গৌড়েশ্বরে। শত কষ্টে রাজা একদিন হারিয়ে গেলেন...... কে ফেরাবে তাকে! শত চেষ্টাতেও রাণী তাকে ফেরাতে পারলেন না।"
+sentence = "১.২ টাকা নিয়ে রাস্তায় নামলাম। রাজপণ্ডিত হব মনে আশা করে। সপ্তশ্লোক ভেটিলাম রাজা গৌড়েশ্বরে। শত কষ্টে রাজা একদিন হারিয়ে গেলেন কে ফেরাবে তাকে! শত চেষ্টাতেও রাণী তাকে ফেরাতে পারলেন না।"
+input_text = re.sub(r'[ঃ।]', ' ', sentence)
 
-input_text = re.sub(r'[,.:@#?!&$।\(\)\[\]\{\}\<\>/|\+\-\*\=\`\~\%\^\\\;\ঃ]', ' ', sentence)
-# print(input_text)
-tokens = input_text.split()
-print(tokens)
+tokens = word_tokenize(input_text)
+
+
+
+new_token = []
+for tk in tokens:
+    if tk not in punctuation:
+        new_token.append(tk)
+        
+print(new_token)
 
 """
-output: ['রাজপণ্ডিত', 'হব', 'মনে', 'আশা', 'করে', 'সপ্তশ্লোক', 'ভেটিলাম', 'রাজা', 'গৌড়েশ্বরে', 'শত', 'কষ্টে', 'রাজা', 
-'একদিন', 'হারিয়ে', 'গেলেন', 'কে', 'ফেরাবে', 'তাকে', 'শত', 'চেষ্টাতেও', 'রাণী', 'তাকে', 'ফেরাতে', 'পারলেন', 'না']
+['১.২', 'টাকা', 'নিয়ে', 'রাস্তায়', 'নামলাম', 'রাজপণ্ডিত', 'হব', 'মনে', 'আশা', 'করে', 'সপ্তশ্লোক', 
+'ভেটিলাম', 'রাজা', 'গৌড়েশ্বরে', 'শত', 'কষ্টে', 'রাজা',
+'একদিন', 'হারিয়ে', 'গেলেন', 'কে', 'ফেরাবে', 'তাকে', 'শত',
+'চেষ্টাতেও', 'রাণী', 'তাকে', 'ফেরাতে', 'পারলেন', 'না']
 """
